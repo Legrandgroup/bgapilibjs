573c05cd55746ca90e9eeaab2fdc64de449cec7c
Lionel AINS <lionel.ains@legrand.fr>
2019-07-17 09:57:19 +0200

    Moving Commands to a dedicated class in bgapi-commands.js

---
+++ bgapi-commands.js
@@ -0,0 +1,64 @@
+const bgapiDefs = require('./bgapi-defs.js');
+//const bgapiErrors = require('./bgapi-errors.js');
+//const bgapiUtils = require('./bgapi-utils.js');
+
+/**
+ * @brief Implementation of command gatt_server_write_attribute_value
+ * @param attribute A 16-bit value containing the Attribute handle
+ * @param offset A 16-bit value containint the Value offset
+ * @param value A variable-length Buffer object containing the Value
+**/
+function cmd_gatt_server_write_attribute_value(attribute, offset, value) {
+  if (typeof value == 'number')  /* apply() method invoked on handler changes buffer into a serie of byte arguments */
+    value = Buffer.from(arguments, 2);  /* if this is the case, convert arguments back to a Buffer object to be able to process it, skipping the previous positional arguments */
+  if (typeof value == 'string') /* We also permissively accept a string as value (3rd parameter), we will then perform the transformation to a buffer ourselves */
+    value = Buffer.from(value);
+  resultHead = Buffer.alloc(5); /* Allocate twice 16-bits to carry attribute + offset, and one more byte to store the length of the byte array for value */
+  resultHead.writeUInt16LE(attribute, 0);
+  resultHead.writeUInt16LE(offset, 2);
+  if (value.length>255)
+    throw new Error('Value longer than 255 bytes: ' + value.toString('hex'));
+  resultHead.writeUInt8(value.length, 4);
+  return Buffer.concat([resultHead, value]);
+}
+
+/**
+ * @brief List of known command messages, expected encoding, and specific handlers if any
+ *
+ * Keys are the command official name as a string (taken from the BGAPI spec, but without the 'cmd_' prefix) (ie: cmd_system_reset is called 'system_reset')
+ * In each entry for a specific key, we will store a object describing the command:
+ * - Attribute id is mandatory and contains the 1-byte command ID (taken from the BGAPI spec, aka method)
+ * - Attribute minimumPayloadLength contains the expected minimumPayloadLength for this command (aka lolen). This will be assumed to be 0 if not provided
+ * - Attrivute classId contains the class ID for this command (taken from the BGAPI spec, aka class). If not provided, it will be guessed from the prefix of command name using PrefixToClass above
+ * - Attribute handler points to a function that will process and encode the command arguments, it must return a Buffer object containing the added payload of the command (excluding the fixed 4 BGAPI header bytes)
+**/
+const Commands = {
+  'system_reset' : {
+    id : 0x01,
+    minimumPayloadLength : 1,
+    handler : function(dfu) {
+        if (dfu<0 && dfu>2)
+            throw new Error("Invalid dfu value: " + dfu);
+        return Buffer.from([dfu]);
+    }
+  },
+  'system_get_bt_address' : { id : 0x03 },
+  'flash_ps_erase_all' : { id : 0x01 },
+  'mesh_node_init' : { id : 0x00 },
+  'mesh_node_start_unprov_beaconing' : {
+    id: 0x01,
+    minimumPayloadLength : 1,
+    handler : function(bearer) {
+      return Buffer.from([bearer]);
+    }
+  },
+  'gatt_server_write_attribute_value' : {
+    id : 0x02,
+    minimumPayloadLength : 5,
+    handler : cmd_gatt_server_write_attribute_value,
+  },
+  'mesh_generic_client_init' : { id : 0x04 },
+  'mesh_generic_server_init' : { id : 0x04 },
+}
+
+module.exports.Commands = Commands;
--- bgapilib.js
+++ bgapilib.js
@@ -1,6 +1,7 @@
 const bgapiDefs = require('./bgapi-defs.js');
 const bgapiResponses = require('./bgapi-responses.js');
 const bgapiEvents = require('./bgapi-events.js');
+const bgapiCommands = require('./bgapi-commands.js');
 const bgapiUtils = require('./bgapi-utils.js');
 
 const DEBUG = false;  /* Set this to true to enable verbose debug to console */
@@ -26,65 +27,6 @@ function namePrefixToClassId(name) {
 }
 
 /**
- * @brief Implementation of command gatt_server_write_attribute_value
- * @param attribute A 16-bit value containing the Attribute handle
- * @param offset A 16-bit value containint the Value offset
- * @param value A variable-length Buffer object containing the Value
-**/
-function cmd_gatt_server_write_attribute_value(attribute, offset, value) {
-  if (typeof value == 'number')  /* apply() method invoked on handler changes buffer into a serie of byte arguments */
-    value = Buffer.from(arguments, 2);  /* if this is the case, convert arguments back to a Buffer object to be able to process it, skipping the previous positional arguments */
-  if (typeof value == 'string') /* We also permissively accept a string as value (3rd parameter), we will then perform the transformation to a buffer ourselves */
-    value = Buffer.from(value);
-  resultHead = Buffer.alloc(5); /* Allocate twice 16-bits to carry attribute + offset, and one more byte to store the length of the byte array for value */
-  resultHead.writeUInt16LE(attribute, 0);
-  resultHead.writeUInt16LE(offset, 2);
-  if (value.length>255)
-    throw new Error('Value longer than 255 bytes: ' + value.toString('hex'));
-  resultHead.writeUInt8(value.length, 4);
-  return Buffer.concat([resultHead, value]);
-}
-
-/**
- * @brief List of known command messages, expected encoding, and specific handlers if any
- *
- * Keys are the command official name as a string (taken from the BGAPI spec, but without the 'cmd_' prefix) (ie: cmd_system_reset is called 'system_reset')
- * In each entry for a specific key, we will store a object describing the command:
- * - Attribute id is mandatory and contains the 1-byte command ID (taken from the BGAPI spec, aka method)
- * - Attribute minimumPayloadLength contains the expected minimumPayloadLength for this command (aka lolen). This will be assumed to be 0 if not provided
- * - Attrivute classId contains the class ID for this command (taken from the BGAPI spec, aka class). If not provided, it will be guessed from the prefix of command name using PrefixToClass above
- * - Attribute handler points to a function that will process and encode the command arguments, it must return a Buffer object containing the added payload of the command (excluding the fixed 4 BGAPI header bytes)
-**/
-const Commands = {
-  'system_reset' : {
-    id : 0x01,
-    minimumPayloadLength : 1,
-    handler : function(dfu) {
-        if (dfu<0 && dfu>2)
-            throw new Error("Invalid dfu value: " + dfu);
-        return Buffer.from([dfu]);
-    }
-  },
-  'system_get_bt_address' : { id : 0x03 },
-  'flash_ps_erase_all' : { id : 0x01 },
-  'mesh_node_init' : { id : 0x00 },
-  'mesh_node_start_unprov_beaconing' : {
-    id: 0x01,
-    minimumPayloadLength : 1,
-    handler : function(bearer) {
-      return Buffer.from([bearer]);
-    }
-  },
-  'gatt_server_write_attribute_value' : {
-    id : 0x02,
-    minimumPayloadLength : 5,
-    handler : cmd_gatt_server_write_attribute_value,
-  },
-  'mesh_generic_client_init' : { id : 0x04 },
-  'mesh_generic_server_init' : { id : 0x04 },
-}
-
-/**
  * @brief This function creates a BGAPI byte buffer representing the command provided as first argument
  *
  * @param commandName The official name of the command in the BGAPI spec
@@ -93,18 +35,18 @@ const Commands = {
  * @note If the command requires additional arguments, you can provide them as 2nd, 3rd etc. arguments, for example: getCommand('system_reset', 0)
 **/
 function getCommand(commandName) {
-    if (Commands[commandName]) {
+    if (bgapiCommands.Commands[commandName]) {
       let header = Buffer.alloc(4);
       
       header[0] = bgapiDefs.MessageTypes.Command;
       
-      let minimumPayloadLength = Commands[commandName].minimumPayloadLength;
+      let minimumPayloadLength = bgapiCommands.Commands[commandName].minimumPayloadLength;
       if (minimumPayloadLength === undefined) { /* Assume 0 if no minimumPayloadLength property exists */
         minimumPayloadLength = 0;
       }
       header[1] = minimumPayloadLength;
       
-      let classId = Commands[commandName].classId;
+      let classId = bgapiCommands.Commands[commandName].classId;
       if (classId === undefined) {  /* If no classId is present in the Commands object, then try to guess based on the command prefix */
         classId = namePrefixToClassId(commandName);
       }
@@ -113,15 +55,15 @@ function getCommand(commandName) {
       }
       header[2] = classId
       
-      header[3] = Commands[commandName].id;
+      header[3] = bgapiCommands.Commands[commandName].id;
       
-      payloadHandler = Commands[commandName].handler;
+      payloadHandler = bgapiCommands.Commands[commandName].handler;
       let message;
       if (payloadHandler === undefined) { /* If there is no payload handler, generate no payload */
         message = header;
       }
       else {
-        let payload = Commands[commandName].handler.apply(this, Array.prototype.slice.call(arguments, 1));
+        let payload = bgapiCommands.Commands[commandName].handler.apply(this, Array.prototype.slice.call(arguments, 1));
         message = Buffer.concat([header, payload])
       }
       if (DEBUG) console.debug('Packet is ' + message.toString('hex'));
